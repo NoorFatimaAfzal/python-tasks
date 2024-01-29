01f1bc4e84322c7ab454fb00d24afdfc7ae8b8ee
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-01-29 14:46:37 +0500

    tasks added

--- week2.py/task1.py
+++ week2.py/task1.py
@@ -1,6 +1,6 @@
-bimaryNum=input("enter a number")
+binaryNum=input("enter a number")
 decimal=0
-for i in range(len(bimaryNum)):
-    power=len(bimaryNum)-1-i
-    decimal+=(int(bimaryNum[i])*(2)**(power))
+for i in range(len(binaryNum)):
+    power=len(binaryNum)-1-i
+    decimal+=(int(binaryNum[i])*(2)**(power))
 print(decimal)
