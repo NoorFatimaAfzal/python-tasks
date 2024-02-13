84ab435ac8acc96a8b0996756d474b3ddab55aaf
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-14 01:49:49 +0500

    working on project

---
+++ ECAT_app.py/project1.py
@@ -0,0 +1,12 @@
+questions=[["Which one is not a branch of physical sciences?"],
+           ["Which branch of science plays an important role in the development of technology and engineering?"],
+           ["The number of categories in which physical quantities are divided are"],
+           ["How many types of units are in SI?"],
+           ["In scientific notation numbers are expressed in"]]
+options=[["A.chemistry","B.astronomy","C.geology","D.biology"],
+         ["A.chemistry","B.physics","C.geology","D.biology"],
+         ["A.one","B.two","C.three","D.four"],
+         ["A.one","B.two","C.three","D.four"],
+         ["A.powe of 10","B.power of 2","C.reciprocal","D.decimal"]]
+answers=["D","B","B","C","A"]
+
