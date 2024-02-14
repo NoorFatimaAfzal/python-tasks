ca2bda4e6f7af9ba37015e718c809d8c5e0e3d01
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-14 16:21:15 +0500

    still working

--- ECAT_app.py/project1.py
+++ ECAT_app.py/project1.py
@@ -1,36 +1,24 @@
 questions = [
-    ["Which one is not a branch of physical sciences?"],
-    ["Which branch of science plays an important role in the development of technology and engineering?"],
-    ["The number of categories in which physical quantities are divided are"],
-    ["How many types of units are in SI?"],
-    ["In scientific notation numbers are expressed in"]
-]
-options = [
-    ["A.chemistry", "B.astronomy", "C.geology", "D.biology"],
-    ["A.chemistry", "B.physics", "C.geology", "D.biology"],
-    ["A.one", "B.two", "C.three", "D.four"],
-    ["A.one", "B.two", "C.three", "D.four"],
-    ["A.power of 10", "B.power of 2", "C.reciprocal", "D.decimal"]
+    ["Which one is not a branch of physical sciences?\nA. chemistry\nB. astronomy\nC. geology\nD. biology"],
+    ["Which branch of science plays an important role in the development of technology and engineering?A. chemistry\nB. physics\nC. geology\nD. biology"],
+    ["The number of categories in which physical quantities are divided are\nA. one\nB. two\nC. three\nA. one\nB. two\nC. three\nD. four"],
+    ["How many types of units are in SI?\nA. one\nB. two\nC. three\nD. four"],
+    ["In scientific notation numbers are expressed in\nA. power of 10\nB. power of 2\nC. reciprocal\nD. decimal"]
 ]
 answers = ["D", "B", "B", "C", "A"]
-choices = [["submit"], ["skip"]]
+choices = [["select any option(A B C D)\tor\tsubmit\tor\tskip"]]
 question_no = 0
 score = 0
 
 for i in range(len(questions)):
     for question in questions[i]:
         print(question)
+        print(choices[0])
+        choice = input("Enter your choice: ").upper()
+        if choice == answers[question_no]:
+            score += 1
+        if choice == "SUBMIT":
+            break
         question_no += 1
-        for option in options[question_no]:
-            print(option)
-            ans = input("select any option(A B C D)").upper()
-            if ans == answers[question_no]:
-                score += 1
-                break
-            choice1 = input("Skip ? y/n").lower()
-            if choice1 == "y":
-                break
-            choice2 = input("Submit ? y/n").lower()
-            if choice2 == "y":
-                break
-        
+
+print("Your score is:", score)
