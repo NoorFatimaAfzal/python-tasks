45b7c4d6e543bff651c6eb77266037c90d1f15ae
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-14 12:08:06 +0500

    still working on project

--- ECAT_app.py/project1.py
+++ ECAT_app.py/project1.py
@@ -9,4 +9,67 @@ options=[["A.chemistry","B.astronomy","C.geology","D.biology"],
          ["A.one","B.two","C.three","D.four"],
          ["A.powe of 10","B.power of 2","C.reciprocal","D.decimal"]]
 answers=["D","B","B","C","A"]
+choices=[["submit"],["skip"]]
+question_no=0
+score=0
+for i in range(len(questions)):
+    for question in questions[i]:
+        print(question)
+        for option in options[question_no]:
+            print(option)
+            ans=input("select any option(A B C D)").upper()
+            if ans==answers[question_no]:
+                score+=1
+                break
+            choice1=input("Skip ? y/n").lower()
+            if choice1=="y":
+                break
+            choice2=input("Submit ? y/n").lower()
+            if choice2=="y":
+                break
+        question_no+=1
 
+            
+    # user_ans=input("select any option").upper()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# question_no=0
+# score=0
+# user_answers=[]
+# for question in questions:
+#     print(question)
+#     for option in options[question_no]:
+#         print(option)
+#     user_ans=input("select any option").upper()
+#     user_answers.append(user_ans)
+#     if user_ans==user_answers[question_no]:
+#         score+=1
+#     question_no+=1
+# print(score)
