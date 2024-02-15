9f9295ddff5a1028c554f71a1822445c19a1f8e6
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-15 20:30:27 +0500

    working on project

---
+++ ECAT_app.py/imageViewer.py
@@ -0,0 +1,55 @@
+from tkinter import *
+from PIL import ImageTk, Image
+root=Tk()
+
+root.title("Image Viewer")
+root.iconbitmap('ECAT_app.py/n.ico')
+my_image1=ImageTk.PhotoImage(Image.open("ECAT_app.py/1.jpg"))
+my_image2=ImageTk.PhotoImage(Image.open("ECAT_app.py/2.jpg"))
+my_image3=ImageTk.PhotoImage(Image.open("ECAT_app.py/3.jpg"))
+my_image4=ImageTk.PhotoImage(Image.open("ECAT_app.py/4.jpg"))
+my_image5=ImageTk.PhotoImage(Image.open("ECAT_app.py/5.jpg"))
+my_image6=ImageTk.PhotoImage(Image.open("ECAT_app.py/6.jpg"))
+
+image_list=[my_image1, my_image2, my_image3, my_image4, my_image5, my_image6]
+
+my_label=Label(image=my_image1)
+my_label.grid(row=0, column=0, columnspan=3)
+
+def forward():
+    global my_label
+    global button_forward
+    global button_back
+    my_label.grid_forget() #to remove the previous image
+    my_label=Label(image=image_list[1])
+    button_forward=Button(root, text=">>", command=forward)
+    button_back=Button(root, text="<<", command=back)
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_exit.grid(row=1, column=1)
+    button_forward.grid(row=1, column=2)
+
+def back():
+    global my_label
+    global button_forward
+    global button_back
+    my_label.grid_forget()
+    my_label=Label(image=image_list[0])
+    button_forward=Button(root, text=">>", command=forward)
+    button_back=Button(root, text="<<", command=back)
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_exit.grid(row=1, column=1)
+    button_forward.grid(row=1, column=2)
+
+button_back=Button(root, text="<<", command=back)
+button_exit=Button(root, text="Exit", command=root.quit)
+button_forward=Button(root, text=">>",command=lambda: button_forward(2))
+
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
+
+
+
+root.mainloop()
--- ECAT_app.py/images.py
+++ ECAT_app.py/images.py
@@ -1,34 +1,16 @@
 from tkinter import *
+from PIL import ImageTk, Image
 root=Tk()
-#i want to make a quiz app
-question=Label(root, text="What is the capital of France?", font=("Arial", 20))
-question.pack()
-def check(number):
-    if number==1:
-        print("Correct")
-    else:
-        print("Incorrect")
-        
+
+root.title("learning tkinter")
+root.iconbitmap('ECAT_app.py/n.ico')
 
 
-#make buttons
-button1=Checkbutton(root, text="Paris", padx=40, pady=20,command=lambda: check(1))
-button2=Checkbutton(root, text="London", padx=40, pady=20,command=lambda: check(2))
-button3=Checkbutton(root, text="Berlin", padx=40, pady=20,command=lambda: check(3))
-button4=Checkbutton(root, text="Madrid", padx=40, pady=20,command=lambda: check(4))
-button1.pack()
-button2.pack()
-button3.pack()
-button4.pack()
-#make a skip button
-skip_button=Button(root, text="Skip", padx=40, pady=20, )
-skip_button.pack()
-#make a submit button
-submit_button=Button(root, text="Submit", padx=40, pady=20)
-submit_button.pack()
+my_img1=ImageTk.PhotoImage(Image.open("ECAT_app.py/n.ico"))
+my_label=Label(image=my_img1) 
+my_label.pack()
 
-
-
-
+quit_button=Button(root, text="Exit", command=root.quit)
+quit_button.pack()
 
 root.mainloop()
