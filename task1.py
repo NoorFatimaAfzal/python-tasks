5524adb0df3a84e442cf4e51343012aeed2b69cb
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-23 11:53:07 +0500

    sets and dictionaries

--- task1.py
+++ task1.py
@@ -1,6 +1,5 @@
-s={1,3.6,"Uet"}
-s1=set([44.5,"UET",7])
-print(s)
-print(s1)
-print(type(s))
-print(type(s1))
+marks={
+"I2C":70, "PF":80, "CA":75
+}
+for sub,mark in marks.items():
+    print(sub,mark)
