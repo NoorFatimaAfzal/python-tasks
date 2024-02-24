b1c6a180413fe0a6f1ae148bc1b0a74f1a4ef958
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-24 15:08:08 +0500

    2nd task done

--- task2.py
+++ task2.py
@@ -1,11 +1,14 @@
 paragraph=input("enter a paragraph")
 lst_of_words=paragraph.split(" ")
-print(len(lst_of_words))
-
+print(f"no of words in the given paragraph are : len(lst_of_words)")
 
 new_dict={}
-new_dict.update(word)
-# for word in lst_of_words:
-#     if word not in new_dict:
-#         new_dict.update(word)
-# print(new_dict)        
+n=1
+for word in lst_of_words:
+    if word in new_dict:
+        new_dict.update({word:n+1})
+        
+    if word not in new_dict:
+        new_dict.update({word:n})
+    
+print(new_dict)
