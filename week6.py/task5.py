82b0dd68e4b90c7c33e0cb4d3ff3b6e10c9932da
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-02-28 23:44:43 +0500

    sum of list

--- week6.py/task5.py
+++ week6.py/task5.py
@@ -1,8 +1,21 @@
-def invert(string,i):
-    l=len(string)
-    if l>0:
-        print(string[l-1],end="")
-        invert(string[l-1])
-string=input("enter string: ")        
-i=len(string)        
-invert(string,i)
+# def invert(string,i):
+#     l=len(string)
+#     if l>0:
+#         print(string[l-1],end="")
+#         invert(string[l-1])
+# string=input("enter string: ")        
+# i=len(string)        
+# invert(string,i)
+
+def list_sum(num_List):
+    # Check if the length of the input list is 1
+    if len(num_List) == 1:
+        # If the list has only one element, return that element
+        return num_List[0]
+    else:
+        # If the list has more than one element, return the sum of the first element
+        # and the result of recursively calling the list_sum function on the rest of the list
+        return num_List[0] + list_sum(num_List[1:])
+
+# Print the result of calling the list_sum function with the input [2, 4, 5, 6, 7]
+print(list_sum([2, 4, 5, 6, 7]))
