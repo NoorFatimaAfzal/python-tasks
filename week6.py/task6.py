9a4156864aa171da5c161538512b5054e4add1eb
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-03-02 22:06:49 +0500

    tasks

--- week6.py/task6.py
+++ week6.py/task6.py
@@ -1,12 +1,10 @@
 def only(lst):
     if not lst:
         return []
-    
     if type(lst[0])==str:
         return [lst[0]] + only(lst[1:])
     else:
         return only(lst[1:])
-
 lst = [1, "noor","sjh", [1, 2], ("noor",), {"noor"}, "hjdshj"]
 new_lst = only(lst)
 print(new_lst)
