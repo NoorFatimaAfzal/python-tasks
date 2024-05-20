6efaac2f8a096f0bf03657db4c29556aa1ba0d5e
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-05-20 05:29:08 +0500

    task updatd

--- week5.py/task1.py
+++ week5.py/task1.py
@@ -1,6 +1,7 @@
 engineering={"A","B","C","D","E","F"}
 no_enggineering={"E","F","G","H"}
 both=engineering.intersection(no_enggineering)
+
 print(f"students who applied in both engineering and non engineering are {both}")
 all=engineering.union(no_enggineering)
 list(all)
