ed6b4d2cc4263641efa7f6942f6fd95cfdca27c6
Noor Fatima <154274914+NoorFatimaAfzal@users.noreply.github.com>
2024-05-23 02:19:30 +0500

    updated

--- week2.py/task3.py
+++ week2.py/task3.py
@@ -2,6 +2,7 @@ lowerRange = int(input("Lower value of the range: "))
 upperRange = int(input("Upper value of the range: "))
 PCount = 0
 sum=0
+
 div=2
 for num in range(lowerRange,upperRange+1):   
     for j in range(2, num):
